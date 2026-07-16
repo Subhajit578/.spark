027516af2a1f4d75bd79f21d2c2c2a5877b7e739
Subhajit578 <123349280+Subhajit578@users.noreply.github.com>
2026-07-16 02:11:16 -0500

    Package Manager changes

--- apps/code-server/start.sh
+++ apps/code-server/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-node /home/coder/worker-backend/dist/index.js &
+node /home/coder/apps/worker-backend/dist/index.js &
 code-server --auth none --bind-addr 0.0.0.0:8080 /tmp/spark-worker
