b37277d33a3c1c91e58433c852922b3f37200af6
Subhajit578 <123349280+Subhajit578@users.noreply.github.com>
2026-07-17 00:04:59 -0500

    fix rewrite prefix to /backend

--- apps/frontend-next/config.ts
+++ apps/frontend-next/config.ts
@@ -1,2 +1,2 @@
-export const BASE_BACKEND_URL = process.env.NEXT_PUBLIC_BASE_BACKEND_URL || '/api'
+export const BASE_BACKEND_URL = process.env.NEXT_PUBLIC_BASE_BACKEND_URL || '/backend'
 export const WORKER_URL = process.env.NEXT_PUBLIC_WORKER_URL || 'http://localhost:3001'
--- apps/frontend-next/next.config.ts
+++ apps/frontend-next/next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
+        source: '/backend/:path*',
         destination: `${process.env.BACKEND_URL ?? 'http://localhost:3000'}/:path*`,
       },
     ]
